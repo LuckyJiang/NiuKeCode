ca4a33e57737f90d3b97db383ae22ed1d2d4f35c
jiangxiaomin <jxm123>
2021-04-07 14:43:08 +0800

    系统登录：记录可参考的文章

---
+++ Login/src/main/java/com/min/login/LoginApplication.java
@@ -0,0 +1,23 @@
+package com.min.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ***：https://blog.csdn.net/pingpei1133/article/details/109846267
+ * ***：https://blog.csdn.net/li3455277925/article/details/108350520
+ * ***：https://blog.csdn.net/fangjun_lj/article/details/81410768
+ * ***：https://blog.csdn.net/pingpei1133/article/details/109846267
+ * ***：https://blog.csdn.net/piisabutcher/article/details/103278897?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-3.control&dist_request_id=1328767.81078.16177628114573619&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-3.control
+ *
+ *
+ * https://javazhiyin.blog.csdn.net/article/details/107873082
+ */
+@SpringBootApplication
+public class LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApplication.class, args);
+	}
+
+}
