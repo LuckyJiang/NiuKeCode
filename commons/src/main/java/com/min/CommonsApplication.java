22cd4081dc4d2ef9815c0074350684164c6245ab
jiangxiaomin <jxm123>
2021-04-25 13:09:44 +0800

    yml 配置：map,list

--- commons/src/main/java/com/min/CommonsApplication.java
+++ commons/src/main/java/com/min/CommonsApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CommonsApplication {
 
+	/**
+	 * https://blog.csdn.net/fanzhongli/article/details/84903157?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-2.control&dist_request_id=1332042.22617.16193272262187849&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-2.control
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CommonsApplication.class, args);
 	}
