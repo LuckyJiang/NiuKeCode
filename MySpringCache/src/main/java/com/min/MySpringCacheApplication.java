33c874930160a506a23ed0d47b493b115b23d7ab
jiangxiaomin <jxm123>
2021-04-26 15:56:03 +0800

    springcache

---
+++ MySpringCache/src/main/java/com/min/MySpringCacheApplication.java
@@ -0,0 +1,17 @@
+package com.min;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringCacheApplication {
+
+	/**
+	 * https://blog.csdn.net/justry_deng/article/details/89283664
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringCacheApplication.class, args);
+	}
+
+}
