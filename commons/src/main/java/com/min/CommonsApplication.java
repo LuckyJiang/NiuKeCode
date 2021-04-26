6f76bfa8c54e5cd5fb7d8d2e2af3e5efeae9cfc8
jiangxiaomin <jxm123>
2021-04-26 13:52:48 +0800

    yml 配置：map,list

--- commons/src/main/java/com/min/CommonsApplication.java
+++ commons/src/main/java/com/min/CommonsApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommonsApplication {
 
 	/**
+	 * application.yml中配置各种类型的数据：list map
 	 * https://blog.csdn.net/fanzhongli/article/details/84903157?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-2.control&dist_request_id=1332042.22617.16193272262187849&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-2.control
+	 * https://blog.csdn.net/majinggogogo/article/details/104455408?utm_medium=distribute.pc_relevant_download.none-task-blog-2~default~BlogCommendFromBaidu~default-2.nonecase&dist_request_id=&depth_1-utm_source=distribute.pc_relevant_download.none-task-blog-2~default~BlogCommendFromBaidu~default-2.nonecas
 	 * @param args
 	 */
 	public static void main(String[] args) {
