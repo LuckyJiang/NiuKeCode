b44d1460e6aedd5a9992bf34928669b744e440f2
jiangxiaomin <jxm123>
2021-07-09 16:42:54 +0800

    pom 配置 maven打包静态资源文件

--- commons/src/main/java/com/min/CommonsApplication.java
+++ commons/src/main/java/com/min/CommonsApplication.java
@@ -12,6 +12,13 @@ public class CommonsApplication {
 	 * https://blog.csdn.net/majinggogogo/article/details/104455408?utm_medium=distribute.pc_relevant_download.none-task-blog-2~default~BlogCommendFromBaidu~default-2.nonecase&dist_request_id=&depth_1-utm_source=distribute.pc_relevant_download.none-task-blog-2~default~BlogCommendFromBaidu~default-2.nonecas
 	 * @param args
 	 */
+
+	/**
+	 * pom.xml 配置静态资源文件
+	 * https://blog.csdn.net/weixin_36550048/article/details/106714656?utm_medium=distribute.pc_relevant.none-task-blog-baidujs_baidulandingword-0&spm=1001.2101.3001.4242
+	 *
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CommonsApplication.class, args);
 	}
